51aca5a6cf5e2650d2730677890014a38aa7a65e
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-09-27 17:30:26 +0200

    refactor: Move code for wave to use before footer into its own FooterWave component

--- components/home/main/ContactSection.tsx
+++ components/home/main/ContactSection.tsx
@@ -1,4 +1,5 @@
 import styles from '../../../styles/ContactSection.module.css';
+import FooterWave from '../../footer/FooterWave';
 
 const ContactSection = () => {
   return (
@@ -25,19 +26,7 @@ const ContactSection = () => {
         </a>
         .
       </h4>
-      <div className={styles.wave}>
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className={styles.shapeFill}
-          ></path>
-        </svg>
-      </div>
+      <FooterWave />
     </section>
   );
 };
--- pages/impressum.tsx
+++ pages/impressum.tsx
@@ -1,4 +1,5 @@
 import Footer from '../components/footer/Footer';
+import FooterWave from '../components/footer/FooterWave';
 import styles from '../styles/Impressum.module.css';
 
 const Impressum = () => {
@@ -123,19 +124,7 @@ const Impressum = () => {
           </p>
         </section>
 
-        <div className={styles.wave}>
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              className={styles.shapeFill}
-            ></path>
-          </svg>
-        </div>
+        <FooterWave />
       </main>
 
       <Footer />
