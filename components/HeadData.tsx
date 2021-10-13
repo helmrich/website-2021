14aa52f95e38a43e5b28352dff88134b6d4d3089
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-10-13 20:00:35 +0200

    chore: Add icons (favicon, etc.) and web manifest

--- components/HeadData.tsx
+++ components/HeadData.tsx
@@ -28,8 +28,25 @@ const HeadData = ({
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       {/* <meta name="twitter:image" content="" /> */}
-      {/* <link rel="icon" href="/favicon.ico" />
-      <link rel="manifest" href="/manifest.json"></link> */}
+      {/* <link rel="manifest" href="/manifest.json"></link> */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <title>{title}</title>
     </Head>
   );
